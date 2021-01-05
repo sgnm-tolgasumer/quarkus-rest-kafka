16d8c8f7930420117315c04efd3cccf137ff5996
sgnm-tolgasumer <tolga.sumer@signumtte.com>
2021-01-05 09:05:19 +0300

    datasource->mysql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,13 @@
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Mango',15);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Strawberry',16);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Orange',17);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Lemon',1);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Blueberry',5);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Banana',7);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Watermelon',8);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Apple',99);
-INSERT INTO Employee(id,name,age) VALUES (nextval('hibernate_sequence'),'Pear',88);
+
+CREATE TABLE EMPLOYEE(id int unsigned not null auto_increment primary key,
+name varchar(30) not null,
+age int unsigned not null);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Mango',15);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Strawberry',16);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Orange',17);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Lemon',1);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Blueberry',5);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Banana',7);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Watermelon',8);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Apple',99);
+INSERT INTO EMPLOYEE(name,age) VALUES ('Pear',88);
